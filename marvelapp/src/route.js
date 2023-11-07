b9d13f07da488a358b7ab08898e8317e20e390fe
MatheoPEROCHON <matheo.perochon@etu.univ-poitiers.fr>
2023-11-07 17:14:39 +0100

    Add build workflow

--- marvelapp/src/route.js
+++ marvelapp/src/route.js
@@ -3,16 +3,35 @@ import AboutPage from "./pages/AboutPages";
 import ContactPage from "./pages/ContactPages";
 import CharactersPage from "./pages/CharactersPages";
 
+import CharacterDetailPage from "./pages/CharacterDetailPage";
+import { getCharacterById, getCharacters } from "./api/character-api";
+
 const route = [
     {
         path: "/",
         element: <Layout />,
         children: [
-            { path: "/", element: <CharactersPage /> },
-            { path: "/about", element: <AboutPage /> },
-            { path: "/contact", element: <ContactPage /> },
+            {
+                path: "/",
+                element: <CharactersPage />,
+                loader: () => getCharacters(),
+            },
+            {
+                path: "/characters/:id",
+                element: <CharacterDetailPage />,
+                loader: ({ params }) => getCharacterById(params.id),
+            },
+            { 
+                path: "/about", 
+                element: <AboutPage /> 
+            },
+            { 
+                path: "/contact", 
+                element: <ContactPage /> 
+            },
         ],
     },
 ];
 
 export default route;
+
