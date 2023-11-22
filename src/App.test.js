8737bf27b66180c031344cc9c9b2b85a6f44542b
MatheoPEROCHON <matheo.perochon@etu.univ-poitiers.fr>
2023-11-22 18:02:38 +0100

    Add sort by and order in CharactersPage

---
+++ src/App.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import { act } from 'react-dom/test-utils';
+
+test('render Marvel App', async () => {
+  await act(() => {
+    render(<App />);
+  });
+
+
+  const h1Element = screen.getByRole('heading', { level: 1, name: "Marvel App" });
+  expect(h1Element).toBeInTheDocument();
+});
