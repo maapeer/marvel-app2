ff965e921ea2cb43ee00777c88ebbb36475608a4
MatheoPEROCHON <matheo.perochon@etu.univ-poitiers.fr>
2023-11-22 18:03:51 +0100

    Add sort by and order in CharactersPage

---
+++ src/route.js
@@ -0,0 +1,46 @@
+import Layout from "./Layout";
+import AboutPages from "./pages/AboutPages";
+import ContactPages from "./pages/ContactPages";
+import CharactersPages from "./pages/CharactersPages";
+
+import CharacterDetailPage from "./pages/CharacterDetailPage";
+import { getCharacterById, getCharacters } from "./api/character-api";
+
+const routes = [
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <CharactersPages />,
+                loader: ({request}) => {
+                    const url = new URL(request.url);
+                    const orderBy = url.searchParams.get("orderBy");
+                    const order = url.searchParams.get("order");
+
+                    if (orderBy && order) {
+                        return getCharacters(orderBy, order);
+                    }else{
+                        return getCharacters();
+                    }
+                },
+            },
+            {
+                path: "/characters/:id",
+                element: <CharacterDetailPage />,
+                loader: ({ params }) => getCharacterById(params.id),
+            },
+            { 
+                path: "/about", 
+                element: <AboutPages /> 
+            },
+            { 
+                path: "/contact", 
+                element: <ContactPages /> 
+            },
+        ],
+    },
+];
+
+export default routes;
